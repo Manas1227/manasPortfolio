8befb450595b2cd0c30b35778c5cba2243fd1831
Manas1227 <manasbhut@gmail.com>
2026-01-31 11:57:38 -0500

    Added waving animation and some css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0deg)" },
+          "15%": { transform: "rotate(14deg)" },
+          "30%": { transform: "rotate(-8deg)" },
+          "40%": { transform: "rotate(14deg)" },
+          "50%": { transform: "rotate(-4deg)" },
+          "60%": { transform: "rotate(10deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+      },
+      animation: {
+        wave: "wave 1.8s infinite ease-in-out",
+      }
+    },
   },
   plugins: [],
 }
